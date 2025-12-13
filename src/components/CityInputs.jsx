098cb662e8d29195172ab8c941d3d6345909b94c
Aidan <lightningzx212@gmail.com>
2025-12-13 14:29:44 -0800

    fix css and div

--- src/components/CityInputs.jsx
+++ src/components/CityInputs.jsx
@@ -5,35 +5,30 @@ export function CityInputs() {
   const { originCity, setOriginCity, currentCity, setCurrentCity } = useContext(WeatherContext);
 
   return (
-    <div class="city-inputs">
-      <div class="input-group">
-        <label>
-          Origin City:
-          <input
-            class="city-input"
-            type="text"
-            value={originCity}
-            onChange={(e) => setOriginCity(e.target.value)}
-          />
-        </label>
-      </div>
-
-      <div class="input-group">
-        <label>
-          Current City:
-          <input
-            class="city-input"
-            type="text"
-            value={currentCity}
-            onChange={(e) => setCurrentCity(e.target.value)}
-          />
-        </label>
-      </div>
-
-      <div style={{ marginTop: "1rem" }}>
-        <p><strong>Origin City:</strong> {originCity}</p>
-        <p><strong>Current City:</strong> {currentCity}</p>
-      </div>
+  <div class="city-inputs">
+    <div class="input-group">
+      <label>
+        Origin City:
+        <input
+          class="city-input"
+          type="text"
+          value={originCity}
+          onChange={(e) => setOriginCity(e.target.value)}
+        />
+      </label>
     </div>
+
+    <div class="input-group">
+      <label>
+        Current City:
+        <input
+          class="city-input"
+          type="text"
+          value={currentCity}
+          onChange={(e) => setCurrentCity(e.target.value)}
+        />
+      </label>
+    </div>
+  </div>
   );
 };
